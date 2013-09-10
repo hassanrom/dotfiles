956b9b33bea6187c7c57a93d3b3ef4d472dffbba
Hassan Rom <hassanrom@gmail.com>
2013-09-10 23:19:17 +0000

    Skip installing configuration files for X if the app is not installed in the system

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ fi
 function backup_if_exist() {
   local f="$1"
   if [[ -e "$f" ]]; then
-    rm "$f.backup"
+    rm -f "$f.backup"
     mv "$f" "$f.backup"
   fi
 }
@@ -28,17 +28,29 @@ backup_if_exist ~/.dir_colors && \
 # Install tmux configuration.
 backup_if_exist ~/.tmux.conf && ln -s dotfiles/tmux.conf ~/.tmux.conf
 
-# Linux specific environment.
+# Linux specific environment when X is installed.
 if [[ $(uname) == *Linux* ]]; then
   # Fluxbox
-  backup_if_exist ~/.fluxbox && ln -s dotfiles/fluxbox ~/.fluxbox
+  if [[ ! -z $(command -v fluxbox) ]]; then
+    backup_if_exist ~/.fluxbox && ln -s dotfiles/fluxbox ~/.fluxbox
+  else
+    echo "Skipped installing fluxbox dotfiles."
+  fi
 
   # Fonts
-  backup_if_exist ~/.fonts && ln -s dotfiles/fonts ~/.fonts
-  fc-cache -fv
+  if [[ ! -z $(command -v fc-cache) ]]; then
+    backup_if_exist ~/.fonts && ln -s dotfiles/fonts ~/.fonts
+    fc-cache -fv
+  else
+    echo "Skipped installing fonts."
+  fi
 
   # Konsole
-  backup_if_exist ~/.kde/share/apps/konsole && \
-    mkdir -p ~/.kde/share/apps &&
-    ln -s dotfiles/konsole ~/.kde/share/apps/konsole
+  if [[ ! -z $(command -v konsole) ]]; then
+    backup_if_exist ~/.kde/share/apps/konsole && \
+      mkdir -p ~/.kde/share/apps &&
+      ln -s dotfiles/konsole ~/.kde/share/apps/konsole
+  else
+    echo "Skipped installing konsole dotfiles."
+  fi
 fi
